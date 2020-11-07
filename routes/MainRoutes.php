a2c2b98e15f29610ef1bdc406d4ad4f469efd2e8
GuilhermeTome <guigo.reis90@gmail.com>
2020-11-07 13:11:36 -0300

    Error route created

--- routes/MainRoutes.php
+++ routes/MainRoutes.php
@@ -1,4 +1,19 @@
 <?php
 
+use Core\Constants\ResponseConstant;
+use Pecee\Http\Request;
+use Pecee\SimpleRouter\Exceptions\NotFoundHttpException;
+use Pecee\SimpleRouter\SimpleRouter;
+
 require PATH_ROUTE . 'app' . DS . 'home.php';
 require PATH_ROUTE . 'app' . DS . 'user.php';
+
+SimpleRouter::error(function(Request $request, \Exception $exception) {
+
+    if($exception instanceof NotFoundHttpException && $exception->getCode() === 404) {
+        return json_response([
+            'msg' => 'Page not found'
+        ], ResponseConstant::NOT_FOUND);
+    }
+    
+});
--- src/Core/Constants/ResponseConstant.php
+++ src/Core/Constants/ResponseConstant.php
@@ -13,6 +13,7 @@ class ResponseConstant
 
     const UNAUTORIZED = 401;
     const FORBIDDEN = 403;
+    const NOT_FOUND = 404;
 
     const INTERNAL_SERVER_ERROR = 500;
 }
